c35e649c34cbb58eaaa60f7b1996fb6847ebe232
petep <petep@AFHI-ITIS25>
2016-06-21 21:48:30 -0400

    Working FTP Download (Still needs to be put in a file)

--- File_Analysis/Program.cs
+++ File_Analysis/Program.cs
@@ -1,41 +1,30 @@
 ﻿using System;
-using System.Windows.Forms;
 using System.Net;
 using System.IO;
+using System.Collections.Generic;
 
 namespace File_Analysis
 {
     class Program
     {
-        public DateTime CurrentDateTime()
-        {
-            DateTime nDate = DateTime.Now;
 
-            return nDate;
+        public static void DownloadFTPFiles()
+        {
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create("ftp://dug/periodic_reports/mga_amp_file_analysis/calendar_day/20160621080000_File%20Analysis_calendar_day.zip");
+            request.Method = WebRequestMethods.Ftp.DownloadFile;
+            request.Credentials = new NetworkCredential("petep", "tr3ple12");
+            FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+            Stream responseStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(responseStream);
+            Console.WriteLine(reader.ReadToEnd());
+            Console.WriteLine("Download Complete, status {0}", response.StatusDescription);
+            reader.Close();
+            response.Close();
         }
 
         static void Main()
         {
-            try
-            {
-                FtpWebRequest request = (FtpWebRequest)WebRequest.Create("ftp://dug/periodic_reports/mga_amp_file_analysis/calendar_day/" + DateTime.Now + "8000*_File Analysis_calendar_day.zip");
-                request.Method = WebRequestMethods.Ftp.DownloadFile;
-                request.Credentials = new
-                NetworkCredential("petep", "tr3ple12");
-                FtpWebResponse response = (FtpWebResponse)request.GetResponse();
-                Stream responseStream = response.GetResponseStream();
-                StreamReader reader = new
-                StreamReader(responseStream);
-                MessageBox.Show(reader.ReadToEnd().ToString());
-                reader.Close();
-                response.Close();
-                Console.WriteLine(reader.ReadToEnd());
-            }
-            catch
-            (NotSupportedException ne)
-            {
-                MessageBox.Show(ne.Message);
-            }
+            DownloadFTPFiles();
         }
     }
 }
